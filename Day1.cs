7b0da5074cd0b70ebfcdcac4db756e72dd91daf8
adavis-io <65918022+adavis-io@users.noreply.github.com>
2022-11-30 22:14:59 -0700

    Add project files.

---
+++ Day1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC22
+{
+    internal class Day1
+    {
+        public 
+    }
+}
---
+++ Solve.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+soln = new Day1();
+
