9f98d4ab1413f60c29bc3ed33c2ae582259dde08
adavis-io <65918022+adavis-io@users.noreply.github.com>
2022-12-09 01:19:33 -0700

    non-working update for part 2. rewriting updatepos

---
+++ Day9.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AOC22
+{
+    public class Day9 : Day
+    {
+        public class Rope
+        {
+            List<(int x, int y)> knots;
+            public Dictionary<(int x, int y), int> visits;
+
+            public Rope(int length)
+            {
+                knots = new();
+                for (int i = 0; i < length; i++)
+                {
+                    knots.Add((0, 0));
+                }
+
+                visits = new();
+                visits.Add((0, 0), 1);
+
+            }
+
+            private void UpdatePos(int i)
+            {
+                var dx = (knots[i - 1].x - knots[i].x);
+                var dy = (knots[i - 1].y - knots[i].y);
+                var distance = Math.Abs(dx) + Math.Abs(dy);
+
+                var tail = knots[i];
+ 
+                if (dx > 1)
+                {
+                    tail.x += 1;
+                }
+                else if (dx < -1)
+                {
+                    tail.x -= 1;
+                }
+                else if (dy > 1)
+                {
+                    tail.y += 1;
+                }
+                else if (dy < -1)
+                {
+                    tail.y -= 1;
+                }
+                if (distance == 3)
+                {
+                    if (Math.Abs(dy) == 2)
+                    {
+                        tail.x += dx;
+                    }
+                    if (Math.Abs(dx) == 2)
+                    {
+                        tail.y += dy;
+                    }
+                }
+
+                knots[i] = tail;
+
+                //Draw(-11, 14, -5, 15);
+                if (i == knots.Count - 1)
+                {
+                    if (!visits.ContainsKey(tail))
+                    {
+                        visits.Add(tail, 1);
+                    }
+                    else
+                    {
+                        visits[tail] += 1;
+                    }
+
+                    return;
+                }
+                else
+                {
+                    UpdatePos(i + 1);
+                }
+            }
+
+            private void ApplyMove(int steps, int dir)
+            {
+                var head = knots[0];
+                for (int i = 0; i < steps; i++)
+                {
+                    switch(dir)
+                    {
+                        case 0:
+                            head.x += 1;
+                            break;
+                        
+                        case 1:
+                            head.y += 1;
+                            break;
+
+                        case 2:
+                            head.x -= 1;
+                            break;
+
+                        case 3:
+                            head.y -= 1;
+                            break;
+                    }
+                    knots[0] = head;
+                    UpdatePos(1);
+                    Draw(0,5,0,4);
+                    //Draw(-11, 14, -5, 15);
+                }
+            }
+            public void Move(string command)
+            {
+                var cmd_split = command.Split(' ');
+                int move_steps = int.Parse(cmd_split[1]);
+                switch(cmd_split[0])
+                {
+                    case "R":
+                        ApplyMove(move_steps, 0);
+                        break;
+
+                    case "U":
+                        ApplyMove(move_steps, 1);
+                        break;
+
+                    case "L":
+                        ApplyMove(move_steps, 2);
+                        break;
+
+                    case "D":
+                        ApplyMove(move_steps, 3);
+                        break;
+
+                }
+                
+            }
+
+            public void Draw(int minx, int maxx, int miny, int maxy)
+            {
+                Console.WriteLine("Current State:");
+
+                for (int j = maxy; j >= miny; j--)
+                {
+                    for (int i = minx; i <= maxx; i++)
+                    {
+                        bool empty = true;
+                        for(int k = 0; k < knots.Count; k++)
+                        {
+                            if(knots[k].x == i && knots[k].y == j)
+                            {
+                                if (k != 0)
+                                {
+                                    Console.Write(k);
+                                }
+                                else
+                                {
+                                    Console.Write('H');
+                                }
+                                empty = false;
+                                break;
+                            }
+                        }
+                        if (empty)
+                        {
+                            if (i == 0 && j == 0)
+                            {
+                                Console.Write('s');
+                            }
+                            else
+                            {
+                                Console.Write('.');
+                            }
+                        }
+                    }
+                    Console.WriteLine("");
+
+                }
+                
+            }
+        }
+
+
+
+        public Day9(bool test) : base(9, test)
+        { 
+        }
+
+        public override void Part1()
+        {
+            Console.Write("\tPart 1: ");
+            var lines = this.Load();
+
+            Rope r = new Rope(2);
+
+            foreach (var line in lines)
+            {
+                r.Move(line);
+            }
+
+            Console.WriteLine(r.visits.Count);
+
+        }
+        public override void Part2()
+        {
+            Console.Write("\tPart 2: ");
+            var lines = this.Load();
+
+            Rope r = new Rope(10);
+
+            Console.WriteLine();
+            r.Draw(0,5,0,4);
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+                r.Move(line);
+                //r.Draw(-11, 14, -5, 15);
+            }
+
+            Console.WriteLine(r.visits.Count);
+        }
+    }
+}
--- Solve.cs
+++ Solve.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 List<AOC22.Day> days = new();
-bool test = false;
+bool test = true;
 
 //days.Add(new AOC22.Day1(test));
 //days.Add(new AOC22.Day2(test));
@@ -10,10 +10,12 @@ bool test = false;
 //days.Add(new AOC22.Day5(test));
 //days.Add(new AOC22.Day6(test));
 //days.Add(new AOC22.Day7(test));
-days.Add(new AOC22.Day8(test));
+//days.Add(new AOC22.Day8(test));
+days.Add(new AOC22.Day9(test));
+
 foreach (var day in days)
 {
     Console.WriteLine(day.name);
-    day.Part1();
+    //day.Part1();
     day.Part2();
 }
